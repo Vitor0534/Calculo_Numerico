f5de062be241c02af97cd26f862246b68bf80d4e
Vitor0534 <75094095+Vitor0534@users.noreply.github.com>
2021-02-09 18:41:55 -0300

    Add files via upload

---
+++ metodo da secante/MetodoSecante.m
@@ -0,0 +1,24 @@
+%metodo da secante%
+
+##p1=input("insira um chute para p1: ");
+##p2=input("\n\n insira um chute para p2: ");
+p1=0.05;
+p2=0.1;
+p3=0;
+FC(p1)
+FC(p2)
+
+if(FC(p1)*FC(p2)<0)
+    i=0;
+    while (i<=3)
+      p3=p2-(FC(p2)*(p2-p1))/(FC(p2)-FC(p1))
+      p2=p3;
+      p1=p2;
+      i++;
+    endwhile
+       
+  else
+        printf("não existe raiz neste intervalo");
+endif
+
+printf("a raiz é %.9f",p3);
